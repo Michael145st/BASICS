2b2ee529854a5d8f40d69fa5b92ad8dcaf6cce0f
Michael145st <119664643+Michael145st@users.noreply.github.com>
2023-07-29 16:35:05 +0200

    Add files via upload

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -27,27 +27,89 @@ function showContent() {
 
 
 
-const tabHeaders = document.querySelectorAll("[data-tab]");
-const tabContent = document.querySelectorAll("[data-tab-content]");
+// Получение элементов, необходимых для работы
+const buttons = document.querySelectorAll('.button-nav');
+const tabs = document.querySelectorAll('.tab');
 
+// Функция для скрытия всех табов и деактивации кнопок
+function hideTabs() {
+  tabs.forEach(tab => {
+    tab.classList.add('hidden');
+  });
 
- tabHeaders.forEach(function (item) {
+  buttons.forEach(button => {
+    button.classList.remove('fo');
+  });
+}
 
-    item.addEventListener("click", function () {
+// Функция для отображения выбранного таба и активации кнопки
+function showTab(tabId) {
+  const tab = document.getElementById(tabId);
+  const button = document.querySelector(`[data-tab="${tabId}"]`);
 
-        tabHeaders.forEach(function (item) {
-            item.classList.add("aktiv");
+  tab.classList.remove('hidden');
+  button.classList.add('fo');
+}
 
-        })
+// Обработчик события при клике на кнопку
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    hideTabs();
+    const tabId = button.getAttribute('data-tab');
+    showTab(tabId);
+  });
+});
 
-        tabContent.forEach(function (item) {
-            item.classList.add("hidden");
+window.onload = function () {
+	var modalContainer = document.getElementById('modal-container')
+	var acceptBtn = document.getElementById('accept-btn')
 
-        })
+	acceptBtn.onclick = function () {
+		modalContainer.style.display = 'none'
+		// Здесь можно добавить логику сохранения соглашения в куки или локальное хранилище
+	}
 
-        const contentBox = document.querySelector("#" + this.dataset.tab);
-        contentBox.classList.remove("hidden");      
-        })
+	// Проверяем, есть ли у пользователя уже сохраненное соглашение по обработке персональных данных
+	var cookieConsent = checkCookieConsent()
 
-    })
-    
+	if (!cookieConsent) {
+		modalContainer.style.display = 'flex'
+	}
+
+	function checkCookieConsent() {
+		// Здесь можно добавить логику проверки наличия сохраненного соглашения в куках или локальном хранилище
+		// Возвращаем true, если соглашение есть, и false в противном случае
+		return false
+	}
+}
+
+document
+	.getElementById('contactForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault() // Предотвращаем отправку формы по умолчанию
+
+		var firstName = document.getElementById('firstName').value
+		var lastName = document.getElementById('lastName').value
+		var phoneNumber = document.getElementById('phoneNumber').value
+		var comment = document.getElementById('comment').value
+
+		// Создаем объект FormData для отправки данных формы на сервер
+		var formData = new FormData()
+		formData.append('firstName', firstName)
+		formData.append('lastName', lastName)
+		formData.append('phoneNumber', phoneNumber)
+		formData.append('comment', comment)
+
+		// Делаем AJAX запрос на сервер для отправки данных формы
+		var xhr = new XMLHttpRequest()
+		xhr.open('POST', 'send_email.php', true)
+		xhr.onload = function () {
+			if (xhr.status === 200) {
+				alert('Форма успешно отправлена!')
+				document.getElementById('contactForm').reset() // Очищаем форму после успешной отправки
+			} else {
+				alert('Произошла ошибка. Пожалуйста, попробуйте еще раз.')
+			}
+		}
+		xhr.send(formData)
+	})
