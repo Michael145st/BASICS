0942368b6163eb8466d85ec97c4bf5ab7eaedec8
Michael145st <119664643+Michael145st@users.noreply.github.com>
2023-08-06 11:03:15 +0200

    Add files via upload

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,50 +1,57 @@
 
-const url = '/api/courses' // URL на базе REST API
+const url = '/api/courses'; // URL на базе REST API
 
 // Функция для выполнения метода GET
 function getCourseData() {
-fetch('https://64ce50fd8bc4d0443865135c--superlative-florentine-17aa90.netlify.app')
+fetch('https://60a3d1917c6e8b0017e27fad.mockapi.io/api/v1/food')
 .then(response => response.json())
 .then(data => {
-displayCourseData(data) // Отображение полученной информации о курсах
+displayCourseData(data); // Отображение полученной информации о курсах
 })
 .catch(error => {
-console.error('Ошибка:', error)
-})
+console.error('Ошибка:', error);
+});
 }
 
 // Функция для отображения полученной информации о курсах
 function displayCourseData(courseData) {
-	const courseCardsContainer = document.getElementById('courseCardsContainer')
+const courseCardsContainer = document.getElementById('courseCardsContainer');
+courseCardsContainer.classList.add('course-list');
 
-	// Очищаем контейнер перед отображением новых карточек
-	courseCardsContainer.innerHTML = ''
 
-	// Создаем карточку курса для каждого элемента в полученных данных
-	courseData.forEach(course => {
-		const courseCard = document.createElement('div')
-		courseCard.classList.add('course-card')
+// Очищаем контейнер перед отображением новых карточек
+courseCardsContainer.innerHTML = '';
 
-		const courseImage = document.createElement('img')
-		courseImage.src = course.image
-		courseImage.alt = course.title
-		courseCard.appendChild(courseImage)
+// Создаем карточку курса для каждого элемента в полученных данных
+courseData.forEach(course => {
+	const courseCard = document.createElement('div');
+	courseCard.classList.add('course-card');
 
-		const courseInfo = document.createElement('div')
-		courseInfo.classList.add('info')
+	const courseFoto = document.createElement('div');
+	courseFoto.classList.add('foto');
 
-		const courseTitle = document.createElement('h2')
-		courseTitle.textContent = course.title
-		courseInfo.appendChild(courseTitle)
+	const courseImage = document.createElement('img');
+	courseImage.src = course.image;
+	courseImage.alt = course.title;
+	courseFoto.appendChild(courseImage);
 
-		const courseDescription = document.createElement('p')
-		courseDescription.textContent = course.description
-		courseInfo.appendChild(courseDescription)
+	const courseInfo = document.createElement('div');
+	courseInfo.classList.add('info');
 
-		
-	})
+	const courseTitle = document.createElement('h2');
+	courseTitle.textContent = course.title;
+	courseInfo.appendChild(courseTitle);
+
+	const courseDescription = document.createElement('p');
+	courseDescription.textContent = course.description;
+	courseInfo.appendChild(courseDescription);
+
+	courseCard.appendChild(courseFoto);
+	courseCard.appendChild(courseInfo);
+
+	courseCardsContainer.appendChild(courseCard);
+});
 }
 
 // Выполняем метод GET для получения информации о курсах при загрузке страницы
-getCourseData()
-
+getCourseData();
